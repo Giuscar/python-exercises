ef0b683bd7aaa64ebb942e5d0aa57292d85ffc18
Giuseppe <giuseppecarella92@yahoo.it>
2020-11-23 23:12:40 +0100

    Added swap case exercise

---
+++ swap_case.py
@@ -0,0 +1,16 @@
+""""
+You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase
+letters and vice versa.
+
+Input Format:
+A single line containing a string S.
+
+Constraints:
+Print the modified string S.
+"""
+def swap_the_string(string_to_swap):
+    print(''.join([c.lower() if c.isupper() else c.upper() for c in string_to_swap]))
+
+if __name__ == "__main__":
+    string_to_swap = "I am A sOftware Engineer"
+    swap_the_string(string_to_swap)
