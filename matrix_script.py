4096394c6ed0f052157674036966f3f72ed007e4
Giuseppe <giuseppecarella92@yahoo.it>
2020-11-25 21:31:53 +0100

    Added matrix_scrip exercise

---
+++ matrix_script.py
@@ -0,0 +1,29 @@
+""""
+Input
+7 3
+Tsi
+h%x
+i #
+sM
+$a
+#t%
+ir!
+
+Output
+This is Matrix#  %!
+"""
+import re
+
+def format_and_print_string(input, list_inputs):
+    n, m = map(int, input.split(' '))
+    a, b = [], ""
+
+    for z in zip(*list_inputs):
+        print(z)
+        b += "".join(z)
+
+    print(re.sub(r"(?<=\w)([^\w]+)(?=\w)", " ", b))
+
+
+if __name__ == "__main__":
+    format_and_print_string("7 3", ["Tsi", "h%x", "i #", "sM ", "$a ", "#t%", "ir!"])
