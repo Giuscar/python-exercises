d1577b025b260b716a42d550a26c24daef732469
Giuseppe <giuseppecarella92@yahoo.it>
2021-03-27 17:52:27 +0100

    Added factory method exercise

---
+++ design_pattern/Factories/factory_method.py
@@ -0,0 +1,38 @@
+from cmath import cos, sin
+from enum import Enum
+
+
+class CoordinatesSystem(Enum):
+    CARTESIAN = 1
+    POLAR = 2
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'x: {self.x} y: {self.y}'
+
+    # def __init__(self, a, b, system=CoordinatesSystem.CARTESIAN):
+    #     if system == CoordinatesSystem.CARTESIAN:
+    #         self.x = a
+    #         self.x = b
+    #     elif system == CoordinatesSystem.POLAR:
+    #         self.x = a * cos(b)
+    #         self.y = a * sin(b)
+
+    @staticmethod
+    def new_cartesian_point(x, y):
+        return Point(x, y)
+
+    @staticmethod
+    def new_polar_point(rho, theta):
+        return Point(rho * cos(theta), rho * sin(theta))
+
+
+if __name__ == "__main__":
+    p = Point(2, 3)
+    p2 = Point.new_polar_point(1, 2)
+    print(p, p2)
