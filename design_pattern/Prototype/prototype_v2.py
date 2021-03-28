23dbd20367898ee010a1b36dea13c4c97b95cc0e
Giuseppe <giuseppecarella92@yahoo.it>
2021-03-28 12:23:11 +0200

    Added n_repeated_elements_in_2N_size_array exercise

--- design_pattern/Prototype/prototype_v2.py
+++ design_pattern/Prototype/prototype_v2.py
@@ -2,10 +2,10 @@ import copy
 
 
 class Address:
-    def __init__(self, stree_address, suite, city):
+    def __init__(self, street_address, suite, city):
         self.suite = suite
         self.city = city
-        self.street_address = stree_address
+        self.street_address = street_address
 
     def __str__(self):
         return f'{self.street_address}, Suite #{self.suite}, {self.city}'
---
+++ n_repeated_elements_in_2N_size_array.py
@@ -0,0 +1,48 @@
+""""
+In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+
+Return the element repeated N times.
+
+
+
+Example 1:
+
+Input: [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: [5,1,5,2,5,3,5,4]
+Output: 5
+
+
+Note:
+
+4 <= A.length <= 10000
+0 <= A[i] < 10000
+A.length is even
+"""
+import collections
+from itertools import groupby
+from typing import List
+
+
+def repeatedNTimesSolution1(A: List[int]) -> int:
+    N = len(A)//2
+    return [int(k) for k, v in groupby(sorted(A))
+            if len(list(v)) == N][0]
+
+
+def repeatedNTimesSolution2(A: List[int]) -> int:
+    count = collections.Counter(A)
+    for k in count:
+        if count[k] > 1:
+            return k
+
+        
+if __name__ == "__main__":
+    print(repeatedNTimesSolution1([1, 2, 3, 3]))
+    print(repeatedNTimesSolution2([1, 2, 3, 3]))
