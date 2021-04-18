5d5b43f803680890834c97b1b043e5703679bff9
Giuseppe <giuseppecarella92@yahoo.it>
2021-04-18 19:00:11 +0200

    Added fibonacci exercise (tabulation version)

---
+++ exercises/dynamic_programming/tabulation/fibonacci.py
@@ -0,0 +1,24 @@
+""""
+Calculate fibonacci series at n-th input position.
+"""
+import numpy as np
+
+
+def calculate_fibonacci_series(n: int) -> int:
+    if n < 0:
+        raise ValueError(str(n) + ' ' + 'should be a positive value')
+
+    tab = np.zeros(n+1)
+    tab[1] = int(1)
+
+    for val in range(2, len(tab)):
+        tab[val] = int(tab[val-1]) + int(tab[val-2])
+    return int(tab[len(tab)-1])
+
+
+if __name__ == '__main__':
+    n = 200
+    try:
+        print(calculate_fibonacci_series(n))
+    except AssertionError:
+        print('Negative value')
