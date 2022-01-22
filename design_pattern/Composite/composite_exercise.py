8105e9b42adc4349a2a1d3f3152bfbc05aef4447
Giuseppe <gcarella228@gmail.com>
2022-01-22 19:48:59 +0100

    update composite exercise

--- design_pattern/Composite/composite_exercise.py
+++ design_pattern/Composite/composite_exercise.py
@@ -23,6 +23,7 @@ class SingleValue:
         self.value = value
 
     def __iter__(self):
+        print("here")
         yield self.value
 
 
@@ -30,6 +31,18 @@ class ManyValues(list, Sum):
     pass
 
 
+if __name__ == '__main__':
+    single_value = SingleValue(11)
+    other_values = ManyValues()
+    other_values.append(22)
+    other_values.append(33)
+    # make a list of all values
+    all_values = ManyValues()
+    all_values.append(single_value)
+    all_values.append(other_values)
+    print(all_values.sum)
+
+"""
 class Evaluate(TestCase):
     def test_exercise(self):
         single_value = SingleValue(11)
@@ -41,4 +54,4 @@ class Evaluate(TestCase):
         all_values.append(single_value)
         all_values.append(other_values)
         self.assertEqual(all_values.sum, 66)
-
+"""
