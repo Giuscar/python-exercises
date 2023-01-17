cebbf87c18a9624680ad1e5b4bc400bc528333c6
Matteo Bertolino <matteobertolino1992@gmail.com>
2023-01-17 10:18:04 +0100

    improve permutation script
    md5 of the contributions:
    - input checks and preprocessing
    - providing a shorter (but less pedagogical) method to solve the problem
    - Doc, code comments and code annotation added
    - added a cool Harry Potter example

--- exercises/permutation.py
+++ exercises/permutation.py
@@ -1,9 +1,33 @@
+#!/usr/bin/env python3
+
 """"
-Given 2 strings, write a method to decide if one is a permutation of the other.
+@Def: Given 2 strings, write a method to decide if one is a permutation of the other.
+
+@Note: the 2 strings are considered anagram if they have the same characters,
+in the same number, but the number of whitespaces/tabs and friends DOES NOT MATTER.
+
+@Example: "dormitory" and "dirty room" are considered anagrams in this exercise.
+
+@Sol: we propose one solution more classical, which calculates the permutations, and
+another one less pedagogical but faster.
 """
 
+import sys 
 
-def calculate_permutation(s1, s2):
+def preproc_input(input_str : str) -> str:
+    """Do some preprocessing - example checks on input, strip, all lowercase and so on"""
+    if not isinstance(input_str, str):
+        err_str = "The input ", input_str, " is not a Python string. Aborting."
+        sys.exit(err_str)
+    input_str = "".join(input_str.lower().split())
+    if len(input_str) == 0:
+        sys.exit("At least one input is an empty string - forbidden. Aborting.")
+    else: 
+        return input_str
+
+
+def calculate_permutation(s1 : str, s2 : str) -> bool:
+    """ Return True whether s1 is a permutation of s2 """
     checker = {}
 
     if len(s1) != len(s2):
@@ -22,11 +46,18 @@ def calculate_permutation(s1, s2):
             checker[idx_value] = checker.get(idx_value) - 1
         else:
             return False
-
     return True
 
 
+def check_anagram_fast(s1 : str, s2 : str) -> bool:
+    """ Alternative to `calculate_permutation` method. Return True whether s1 is an anagram of s2"""
+    return sorted(s1) == sorted(s2)
+
+
 if __name__ == "__main__":
-    s1 = "cicoriandoli"
-    s2 = "cicoriandoli"
-    print("Yes") if calculate_permutation(s1, s2) == True else print("No")
+    s1 = "Tom Marvolo Riddle"
+    s2 = "I am Lord Voldemort"
+    s1 = preproc_input(s1)
+    s2 = preproc_input(s2)
+    print("They are anagrams (classical method)") if calculate_permutation(s1, s2) == True else print("They are NOT anagrams (classical method)")
+    print("They are anagrams (shortcut method)") if check_anagram_fast(s1, s2) == True else print("They are NOT anagrams (shortcut method)")
